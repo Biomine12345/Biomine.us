26d402c987676a2b092329c5e4690168279814bb
Ciara <cicimotl@gmail.com>
2017-04-07 22:38:03 -0400

    index.php

--- index.php
+++ index.php
@@ -6,6 +6,7 @@
         <meta http-equiv="Content-type" content="text/html; charset=utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0">
         <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.6/css/bootstrap.min.css" integrity="sha384-rwoIResjU2yc3z8GV/NPeZWAv56rSmLldC3R/AZzGRnGxQQKnKkoFVhFQhNUwEyJ" crossorigin="anonymous">
+        
         <!--Links for bootstrap4-->
             <script src="https://code.jquery.com/jquery-3.1.1.slim.min.js" integrity="sha384-A7FZj7v+d/sdmMqp/nOQwliLvUsJfDHW+k9Omg/a/EheAdgtzNs3hpfag6Ed950n" crossorigin="anonymous"></script>
             <script src="https://cdnjs.cloudflare.com/ajax/libs/tether/1.4.0/js/tether.min.js" integrity="sha384-DztdAPBWPRXSA/3eYEEUWrWCy7G5KFbe8fFjk5JAIxUYHKkDx6Qin1DkWx51bBrb" crossorigin="anonymous"></script>
@@ -16,23 +17,88 @@
         
         <title>Go Free</title>
     </head>
-    
-    <body>
-            <nav class="navbar fixed-top bg-primary" style="color: white; height: 45px; font-size:15px;">
-                <div class="container">
-                    <div class="row">
-                        <p class="col-md">The Company leading you to a healthy life!</p> 
-                        <div>
-                            <i class="material-icons align-bottom">&#xe55f;</i>41-60 Main Street, Suite 309B, Flushing, NY&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
-                        </div>
-                        <div>
-                            <i class="material-icons align-bottom">&#xe551;</i>(718) 353-8801&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
-                        </div>
-                        <div>
-                            <i class="material-icons align-bottom">&#xe0be;</i>info@biomine.us
-                        </div>
+            <!--header-->
+        <header class="navbar bg-primary" style="color: white; height: 45px; font-size:15px;">
+            <div class="container">
+                <div class="row">
+                    <p class="col-md">The Company leading you to a healthy life!</p> 
+                    <div>
+                        <i class="material-icons align-bottom">&#xe55f;</i>41-60 Main Street, Suite 309B, Flushing, NY&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+                    </div>
+                    <div>
+                        <i class="material-icons align-bottom">&#xe551;</i>(718) 353-8801&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+                    </div>
+                    <div>
+                        <i class="material-icons align-bottom">&#xe0be;</i>info@biomine.us
                     </div>
                 </div>
-            </nav>   
+            </div>
+        </header>   
+    <body>
+
+        <!--End of header-->
+        
+        <!--Nav bar-->
+        <div class="container">
+            <div class="row">
+                <div class="col-md">
+                    <img src="img/Biomne_logo.png" alt="Biomine" style="padding: 15px;">
+                </div>
+                <div class="text-right" style="margin-top: 5%;">
+                    <ul class="nav nav-pills">
+                        <li class="nav-item">
+                            <a class="nav-link" href="#"><strong>HOME</strong></a>
+                        </li>
+                    
+                        <li class="nav-item">
+                            <a class="nav-link" href="#"><strong>ABOUT US</strong></a>
+                        </li>
+                        
+                        <li class="nav-item">
+                            <a class="nav-link" href="#"><strong>CONTACT</strong></a>
+                        </li>
+                        
+                        <li class="nav-item">
+                            <a class="nav-link" href="#"><strong>TESTIMONIALS</strong></a>
+                        </li>
+                    </ul> 
+                </div>
+            </div>
+        </div>
+        <!--End of Navbar-->    
+        
+        <!--Carousel-->
+        <div class="bd-example">
+          <div id="carouselExampleCaptions" class="carousel slide" data-ride="carousel">
+            <ol class="carousel-indicators">
+              <li data-target="#carouselExampleCaptions" data-slide-to="0" class="active"></li>
+              <li data-target="#carouselExampleCaptions" data-slide-to="1"></li>
+            </ol>
+            <div class="carousel-inner" role="listbox">
+              <div class="carousel-item active">
+                <img class="d-block img-fluid" src="img/8products.png" alt="First slide">
+                <div class="carousel-caption d-none d-md-block">
+                  <h3>First slide label</h3>
+                  <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+                </div>
+              </div>
+              <div class="carousel-item">
+                <img class="d-block img-fluid" src="img/jumbo.png" alt="Second slide">
+                <div class="carousel-caption d-none d-md-block">
+                  <h3>Second slide label</h3>
+                  <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+                </div>
+              </div>
+            </div>
+            <a class="carousel-control-prev" href="#carouselExampleCaptions" role="button" data-slide="prev">
+              <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+              <span class="sr-only">Previous</span>
+            </a>
+            <a class="carousel-control-next" href="#carouselExampleCaptions" role="button" data-slide="next">
+              <span class="carousel-control-next-icon" aria-hidden="true"></span>
+              <span class="sr-only">Next</span>
+            </a>
+          </div>
+        </div>
     </body>
 </html>
