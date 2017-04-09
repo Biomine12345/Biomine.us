ac53845f96b2e29878685aa13ee764def3d48e7d
Ciara <cicimotl@gmail.com>
2017-04-09 18:58:43 -0400

    index.php

--- index.php
+++ index.php
@@ -6,6 +6,7 @@
         <meta http-equiv="Content-type" content="text/html; charset=utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0">
         <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.6/css/bootstrap.min.css" integrity="sha384-rwoIResjU2yc3z8GV/NPeZWAv56rSmLldC3R/AZzGRnGxQQKnKkoFVhFQhNUwEyJ" crossorigin="anonymous">
+        <link rel="stylesheet" href="CSS/stylesheet.css" type="text/css">
         
         <!--Links for bootstrap4-->
             <script src="https://code.jquery.com/jquery-3.1.1.slim.min.js" integrity="sha384-A7FZj7v+d/sdmMqp/nOQwliLvUsJfDHW+k9Omg/a/EheAdgtzNs3hpfag6Ed950n" crossorigin="anonymous"></script>
@@ -14,30 +15,35 @@
     
         <!--link for google icons-->
             <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+            
+        <!--links for W3-->
+        <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
+        <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Raleway">
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
         
-        <title>Go Free</title>
+        <title>Biomine - Leading you to a healthier life!</title>
     </head>
-            <!--header-->
-        <header class="navbar bg-primary" style="color: white; height: 45px; font-size:15px;">
-            <div class="container">
-                <div class="row">
-                    <p class="col-md">The Company leading you to a healthy life!</p> 
-                    <div>
-                        <i class="material-icons align-bottom">&#xe55f;</i>41-60 Main Street, Suite 309B, Flushing, NY&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
-                    </div>
-                    <div>
-                        <i class="material-icons align-bottom">&#xe551;</i>(718) 353-8801&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
-                    </div>
-                    <div>
-                        <i class="material-icons align-bottom">&#xe0be;</i>info@biomine.us
-                    </div>
+    
+    <!--header-->
+    <header class="navbar bg-primary" style="color: white; height: 45px; font-size:15px;">
+        <div class="container">
+            <div class="row">
+                <p class="col-md">The Company leading you to a healthy life!</p> 
+                <div>
+                    <i class="material-icons align-bottom">&#xe55f;</i>41-60 Main Street, Suite 309B, Flushing, NY&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+                </div>
+                <div>
+                    <i class="material-icons align-bottom">&#xe551;</i>(718) 353-8801&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+                </div>
+                <div>
+                    <i class="material-icons align-bottom">&#xe0be;</i>info@biomine.us
                 </div>
             </div>
-        </header>   
+        </div>
+    </header>   
+    <!--End of header-->
+    
     <body>
-
-        <!--End of header-->
-        
         <!--Nav bar-->
         <div class="container">
             <div class="row">
@@ -76,7 +82,7 @@
             </ol>
             <div class="carousel-inner" role="listbox">
               <div class="carousel-item active">
-                <img class="d-block img-fluid" src="img/8products.png" alt="First slide">
+                <img class="d-block img-fluid" src="img/capture.png" alt="First slide">
                 <div class="carousel-caption d-none d-md-block">
                   <h3></h3>
                   <p></p>
@@ -105,7 +111,7 @@
         <main>
             <div class="container">
                 <center>
-                    <div class="col-md-12">
+                    <div class="col-md-12" style="padding:2%;">
                         <h2>Enjoy <strong>Our Best Sellers</strong></h2>
                     </div>
                     
@@ -115,22 +121,100 @@
                 
                     <img src="img/Eau-de-rose.jpg" alt="symbol" style="padding: 15px;">
                 </center>
-                <div class="col-4" style="border-style:solid; border-width:1px;">
-                    <center><img src="img/GoFree.PNG" alt="GoFree" style="height:170px; width: 360px; padding: 5px;"><br>
-                    <h1><strong>Go Free</strong></h1>
-                    <p>Go Free is a natural supplement for increasing joint flexibility, supporting joint mobility 
-                        and aiding age-related joint problems. Containing 1500mg of Glucosamine which helps...</p></center>
-                </div>
-                
-                <div class="col-md-3">
-                    
-                </div>
-                
-                <div class="col-md-3">
-                    
-                </div>
-                
             </div>
+            
+            
+            <section id="products">
+                <div class="container">
+                    <!--Beginning of first row-->
+                    <div class="w3-row-padding">
+                        <div class="w3-third w3-container w3-margin-bottom">
+                          <img src="/img/GoFree.PNG" alt="Go Free" style="width:100%" class="w3-hover-opacity">
+                          <div class="w3-container w3-white">
+                              <center><h3><b>Go Free</b></h3>
+                            <p>This proprietary blend of natural ingredients has been formulated to support joint health and flexibility. Containing 1500mg of Glucosamine
+                                which helps keep cartilage in joints healthy, improves recovery from sports injury. Chondroitin reduces pain and improves joint mobility</p></center>
+                          </div>
+                        </div>
+                        <div class="w3-third w3-container w3-margin-bottom">
+                          <img src="/img/Omega3.PNG" alt="Omega3" style="width:100%" class="w3-hover-opacity">
+                          <div class="w3-container w3-white">
+                            <center><h3><b>Omega-3 Fish Oil Blend</b></h3>
+                            <p>Manufactured in a strict quality control environment our Omega 3 Fish Oil is free of all harmful contaminates. Using 100% pure sea-harvested pelagic 
+                                fish oil, making this supplement high in EPA and DHA. These nutrients are important for maintaining a healthy Brain and Nervous System and contributing
+                                to a healthy heart and Joint flexibility.</p></center>
+                          </div>
+                        </div>
+                        <div class="w3-third w3-container">
+                          <img src="/img/LiverSupport.PNG" alt="Liver Support" style="width:100%" class="w3-hover-opacity">
+                          <div class="w3-container w3-white">
+                              <center><h3><b>Liver Support</b></h3>
+                            <p>Liver is the largest internal organ and functions to filter the blood, cleansing toxins, wastes and pathogens. It also plays a key role in cholesterol
+                                balance, digestive enzyme production and energy metabolism. It is the main organ to burn fat. In addition, it is a resilient organ and is the only organ 
+                                capable of regeneration.</p></center>
+                          </div>
+                        </div>
+                    </div>
+                    <!--End of first row-->
+                    
+                    <!--Beginning of second row-->
+                    <div class="w3-row-padding">
+                        <div class="w3-third w3-container w3-margin-bottom">
+                          <img src="/img/MenS.PNG" alt="Go Free" style="width:100%" class="w3-hover-opacity">
+                          <div class="w3-container w3-white">
+                              <center><h3><b>Ultra Vita for Men</b></h3>
+                            <p>This vitamin contains essential vitamins, antioxidants and minerals. It also contains Beta Carotene which supports heart health. In additions
+                                to helping lower high blood pressure, it helps to relieve prostate, macular degeneration, asthma and arthritis symptoms.</p></center>
+                          </div>
+                        </div>
+                        <div class="w3-third w3-container w3-margin-bottom">
+                          <img src="/img/WomenS.PNG" alt="Omega3" style="width:100%" class="w3-hover-opacity">
+                          <div class="w3-container w3-white">
+                            <center><h3><b>Ultra Vita for Women</b></h3>
+                            <p>In today's fast-paced world, many women do not get the daily recommended allowance of vitamins, minerals and other nutrients. Vox Nutrition 
+                                helps to bridge the nutrient gap in your diet by offering a women's multivitamin that supports immune system, increase energy and mental 
+                                alertness to give your body with all that it needs.</p></center>
+                          </div>
+                        </div>
+                        <div class="w3-third w3-container">
+                          <img src="/img/CoQ10.PNG" alt="Liver Support" style="width:100%" class="w3-hover-opacity">
+                          <div class="w3-container w3-white">
+                              <center><h3><b>CoQ10</b></h3>
+                            <p>Necessary for the function of cells, CoQ10 is a powerful antioxidant and vitamin-like compound. Supporting the protection of cell membranes,
+                                particularly after enduring surgery or taking anti-cancer medication, fighting off genetic diseases such as Huntington's Disease and muscular 
+                                dystrophy.</p></center>
+                          </div>
+                        </div>
+                    </div>
+                    <!--End of seecond row-->
+                    
+                    <!--Beginning of third row-->
+                    <div class="w3-row-padding">
+                        <div class="w3-third w3-container w3-margin-bottom">
+                          <img src="/img/Probiotic.PNG" alt="Go Free" style="width:100%" class="w3-hover-opacity">
+                          <div class="w3-container w3-white">
+                              <center><h3><b>Probiotic-40</b></h3>
+                            <p>Probiotic 40 Billion is a blend of 4 strains (40 billion organisms) of Probiotic Bacteria. Our Probiotic supplement is designed to provide a 
+                                high potency and balance of beneficial bacteria. By taking this product on a regular basis Probiotic-40 can help maintain healthy intestinal 
+                                flora. This supplement also contains FOS to improve healthy growth of acidophilus and bifidus organisms.</p></center>
+                          </div>
+                        </div>
+                        <div class="w3-third w3-container w3-margin-bottom">
+                          <img src="/img/SleepFormula.PNG" alt="Omega3" style="width:100%" class="w3-hover-opacity">
+                          <div class="w3-container w3-white">
+                            <center><h3><b>Sleep Formula</b></h3>
+                            <p>This proprietary blend of natural ingredients include  melatonin, camomile and natural vitamin B-6. This natural blend has been formulated to 
+                                aid the body in obtaining a healthy amount of sleep. Sleep formula contains relaxing vitamins, minerals and herbs. When taken as directed, 
+                                sleep formula can help decrease chronic pain and reduce anxiety while falling asleep and sleeping through the night.</p></center>
+                          </div>
+                        </div>
+                    </div>
+                    <!--End of third row-->
+                    
+                </div>
+            </section>
+            
+            
         </main>
     </body>
 </html>
