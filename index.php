ad850dedcaaf2ed187e0e5054bebdad8247cfadf
Ciara <cicimotl@gmail.com>
2017-04-09 21:15:43 -0400

    index.php

--- index.php
+++ index.php
@@ -6,7 +6,7 @@
         <meta http-equiv="Content-type" content="text/html; charset=utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0">
         <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.6/css/bootstrap.min.css" integrity="sha384-rwoIResjU2yc3z8GV/NPeZWAv56rSmLldC3R/AZzGRnGxQQKnKkoFVhFQhNUwEyJ" crossorigin="anonymous">
-        <link rel="stylesheet" href="CSS/stylesheet.css" type="text/css">
+        
         
         <!--Links for bootstrap4-->
             <script src="https://code.jquery.com/jquery-3.1.1.slim.min.js" integrity="sha384-A7FZj7v+d/sdmMqp/nOQwliLvUsJfDHW+k9Omg/a/EheAdgtzNs3hpfag6Ed950n" crossorigin="anonymous"></script>
@@ -20,7 +20,7 @@
         <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
         <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Raleway">
         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
-        
+    
         <title>Biomine - Leading you to a healthier life!</title>
     </head>
     
@@ -84,15 +84,15 @@
               <div class="carousel-item active">
                 <img class="d-block img-fluid" src="img/capture.png" alt="First slide">
                 <div class="carousel-caption d-none d-md-block">
-                  <h3></h3>
-                  <p></p>
+                  
                 </div>
               </div>
               <div class="carousel-item">
                 <img class="d-block img-fluid" src="img/jumbo.png" alt="Second slide">
                 <div class="carousel-caption d-none d-md-block">
-                  <h3></h3>
-                  <p></p>
+                  <h1>Welcome To Biomine!</h1>
+                  <h3>The Company leading you to a healthy life!</h3>
+                  <button type='button' style='color: orange;'>See more products</button>
                 </div>
               </div>
             </div>
@@ -129,7 +129,7 @@
                     <!--Beginning of first row-->
                     <div class="w3-row-padding">
                         <div class="w3-third w3-container w3-margin-bottom">
-                          <img src="/img/GoFree.PNG" alt="Go Free" style="width:100%" class="w3-hover-opacity">
+                            <a href='GoFree.php'><img src="/img/GoFree.PNG" alt="Go Free" style="width:100%" class="w3-hover-opacity"></a>
                           <div class="w3-container w3-white">
                               <center><h3><b>Go Free</b></h3>
                             <p>This proprietary blend of natural ingredients has been formulated to support joint health and flexibility. Containing 1500mg of Glucosamine
@@ -137,7 +137,7 @@
                           </div>
                         </div>
                         <div class="w3-third w3-container w3-margin-bottom">
-                          <img src="/img/Omega3.PNG" alt="Omega3" style="width:100%" class="w3-hover-opacity">
+                          <a href='omega3.php'><img src="/img/Omega3.PNG" alt="Omega3" style="width:100%" class="w3-hover-opacity"></a>
                           <div class="w3-container w3-white">
                             <center><h3><b>Omega-3 Fish Oil Blend</b></h3>
                             <p>Manufactured in a strict quality control environment our Omega 3 Fish Oil is free of all harmful contaminates. Using 100% pure sea-harvested pelagic 
@@ -146,7 +146,7 @@
                           </div>
                         </div>
                         <div class="w3-third w3-container">
-                          <img src="/img/LiverSupport.PNG" alt="Liver Support" style="width:100%" class="w3-hover-opacity">
+                          <a href='Liver_Support.php'><img src="/img/LiverSupport.PNG" alt="Liver Support" style="width:100%" class="w3-hover-opacity"></a>
                           <div class="w3-container w3-white">
                               <center><h3><b>Liver Support</b></h3>
                             <p>Liver is the largest internal organ and functions to filter the blood, cleansing toxins, wastes and pathogens. It also plays a key role in cholesterol
@@ -160,7 +160,7 @@
                     <!--Beginning of second row-->
                     <div class="w3-row-padding">
                         <div class="w3-third w3-container w3-margin-bottom">
-                          <img src="/img/MenS.PNG" alt="Go Free" style="width:100%" class="w3-hover-opacity">
+                            <a href='vita_for_men.php'><img src="/img/MenS.PNG" alt="Go Free" style="width:100%" class="w3-hover-opacity"></a>
                           <div class="w3-container w3-white">
                               <center><h3><b>Ultra Vita for Men</b></h3>
                             <p>This vitamin contains essential vitamins, antioxidants and minerals. It also contains Beta Carotene which supports heart health. In additions
@@ -168,7 +168,7 @@
                           </div>
                         </div>
                         <div class="w3-third w3-container w3-margin-bottom">
-                          <img src="/img/WomenS.PNG" alt="Omega3" style="width:100%" class="w3-hover-opacity">
+                            <a href='vita_for_women.php'><img src="/img/WomenS.PNG" alt="Omega3" style="width:100%" class="w3-hover-opacity"></a>
                           <div class="w3-container w3-white">
                             <center><h3><b>Ultra Vita for Women</b></h3>
                             <p>In today's fast-paced world, many women do not get the daily recommended allowance of vitamins, minerals and other nutrients. Vox Nutrition 
@@ -177,7 +177,7 @@
                           </div>
                         </div>
                         <div class="w3-third w3-container">
-                          <img src="/img/CoQ10.PNG" alt="Liver Support" style="width:100%" class="w3-hover-opacity">
+                            <a href='CoQ10.php'><img src="/img/CoQ10.PNG" alt="Liver Support" style="width:100%" class="w3-hover-opacity"></a>
                           <div class="w3-container w3-white">
                               <center><h3><b>CoQ10</b></h3>
                             <p>Necessary for the function of cells, CoQ10 is a powerful antioxidant and vitamin-like compound. Supporting the protection of cell membranes,
@@ -191,7 +191,7 @@
                     <!--Beginning of third row-->
                     <div class="w3-row-padding">
                         <div class="w3-third w3-container w3-margin-bottom">
-                          <img src="/img/Probiotic.PNG" alt="Go Free" style="width:100%" class="w3-hover-opacity">
+                            <a href='Probiotic.php'><img src="/img/Probiotic.PNG" alt="Go Free" style="width:100%" class="w3-hover-opacity"></a>
                           <div class="w3-container w3-white">
                               <center><h3><b>Probiotic-40</b></h3>
                             <p>Probiotic 40 Billion is a blend of 4 strains (40 billion organisms) of Probiotic Bacteria. Our Probiotic supplement is designed to provide a 
@@ -200,7 +200,7 @@
                           </div>
                         </div>
                         <div class="w3-third w3-container w3-margin-bottom">
-                          <img src="/img/SleepFormula.PNG" alt="Omega3" style="width:100%" class="w3-hover-opacity">
+                            <a href='Sleep_Formula.php'><img src="/img/SleepFormula.PNG" alt="Omega3" style="width:100%" class="w3-hover-opacity"></a>
                           <div class="w3-container w3-white">
                             <center><h3><b>Sleep Formula</b></h3>
                             <p>This proprietary blend of natural ingredients include  melatonin, camomile and natural vitamin B-6. This natural blend has been formulated to 
@@ -214,7 +214,73 @@
                 </div>
             </section>
             
+            <div class="w3-display-container">
+                <img src="/img/pic.PNG" style="width:100%; height: 600px;">
+            </div>
             
         </main>
+        
+        <!-- Subscribe section -->
+        <div class="container-fluid w3-gray w3-padding-32">
+            <div class='container'>
+                <h1>Subscribe</h1>
+                <p>To get special offers and VIP treatment:</p>
+                <p><input class="w3-input w3-border" type="text" placeholder="Enter e-mail" style="width:70%"></p>
+                <button type="button" class="w3-button w3-red w3-margin-bottom">Subscribe</button>
+            </div>
+        </div>
+        
+        
+        
+        <!-- Footer -->
+        <footer class="w3-padding-64 w3-light-grey w3-small w3-center">
+            <div class='container'>
+                <div class="row">
+                   <div class="col-4">
+                        <h4>Contact</h4>
+                        <p>Questions? Go ahead.</p>
+                        <form action="/action_page.php" target="_blank">
+                          <p><input class="w3-input w3-border" type="text" placeholder="Name" name="Name" required></p>
+                          <p><input class="w3-input w3-border" type="text" placeholder="Email" name="Email" required></p>
+                          <p><input class="w3-input w3-border" type="text" placeholder="Subject" name="Subject" required></p>
+                          <p><textarea class="w3-input w3-border" type="text" placeholder="Message" name="Message" required></textarea></p>
+                          <button type="submit" class="w3-button w3-block w3-black">Send</button>
+                        </form>
+                    </div>
+
+                        <div class="col-4">
+                          <h4>About</h4>
+                          <p><a href="#">About us</a></p>
+                          <p><a href="#">We're hiring</a></p>
+                          <p><a href="#">Support</a></p>
+                          <p><a href="#">Find store</a></p>
+                          <p><a href="#">Shipment</a></p>
+                          <p><a href="#">Payment</a></p>
+                          <p><a href="#">Gift card</a></p>
+                          <p><a href="#">Return</a></p>
+                          <p><a href="#">Help</a></p>
+                        </div>
+
+                        <div class="col-4 w3-justify">
+                            <h4>Store</h4>
+                            <p><i class="fa fa-fw fa-map-marker"></i> Company Name</p>
+                            <p><i class="fa fa-fw fa-phone"></i> 0044123123</p>
+                            <p><i class="fa fa-fw fa-envelope"></i> ex@mail.com</p>
+                            <h4>We accept</h4>
+                            <p><i class="fa fa-fw fa-cc-amex"></i> Amex</p>
+                            <p><i class="fa fa-fw fa-credit-card"></i> Credit Card</p>
+                            <br>
+                            <a href="#" class="fa1 fa-facebook"></a>
+                            <a href="#" class="fa1 fa-twitter"></a>
+                            <a href="#" class="fa1 fa-google"></a>
+                            <a href="#" class="fa1 fa-linkedin"></a>
+                            <a href="#" class="fa1 fa-youtube"></a>
+                            <a href="#" class="fa1 fa-instagram"></a>
+                        </div>
+                </center>
+                       
+        </div>
+        </div>
+        </footer>
     </body>
 </html>
