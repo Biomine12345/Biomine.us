2024ad9ba6e0a651f5cedf620d69beccd1dffbd0
Ciara <cicimotl@gmail.com>
2017-04-09 21:17:41 -0400

    Biomine

---
+++ Biomine/index.php
@@ -0,0 +1,286 @@
+<!DOCTYPE HTML>
+<html>
+    <head>
+        <meta charset="utf-8"/>
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta http-equiv="Content-type" content="text/html; charset=utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0">
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.6/css/bootstrap.min.css" integrity="sha384-rwoIResjU2yc3z8GV/NPeZWAv56rSmLldC3R/AZzGRnGxQQKnKkoFVhFQhNUwEyJ" crossorigin="anonymous">
+        
+        
+        <!--Links for bootstrap4-->
+            <script src="https://code.jquery.com/jquery-3.1.1.slim.min.js" integrity="sha384-A7FZj7v+d/sdmMqp/nOQwliLvUsJfDHW+k9Omg/a/EheAdgtzNs3hpfag6Ed950n" crossorigin="anonymous"></script>
+            <script src="https://cdnjs.cloudflare.com/ajax/libs/tether/1.4.0/js/tether.min.js" integrity="sha384-DztdAPBWPRXSA/3eYEEUWrWCy7G5KFbe8fFjk5JAIxUYHKkDx6Qin1DkWx51bBrb" crossorigin="anonymous"></script>
+            <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.6/js/bootstrap.min.js" integrity="sha384-vBWWzlZJ8ea9aCX4pEW3rVHjgjt7zpkNpZk+02D9phzyeVkE+jo0ieGizqPLForn" crossorigin="anonymous"></script>
+    
+        <!--link for google icons-->
+            <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+            
+        <!--links for W3-->
+        <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
+        <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Raleway">
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    
+        <title>Biomine - Leading you to a healthier life!</title>
+    </head>
+    
+    <!--header-->
+    <header class="navbar bg-primary" style="color: white; height: 45px; font-size:15px;">
+        <div class="container">
+            <div class="row">
+                <p class="col-md">The Company leading you to a healthy life!</p> 
+                <div>
+                    <i class="material-icons align-bottom">&#xe55f;</i>41-60 Main Street, Suite 309B, Flushing, NY&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+                </div>
+                <div>
+                    <i class="material-icons align-bottom">&#xe551;</i>(718) 353-8801&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+                </div>
+                <div>
+                    <i class="material-icons align-bottom">&#xe0be;</i>info@biomine.us
+                </div>
+            </div>
+        </div>
+    </header>   
+    <!--End of header-->
+    
+    <body>
+        <!--Nav bar-->
+        <div class="container">
+            <div class="row">
+                <div class="col-md">
+                    <img src="img/Biomne_logo.png" alt="Biomine" style="padding: 15px;">
+                </div>
+                <div class="text-right" style="margin-top: 5%;">
+                    <ul class="nav nav-pills">
+                        <li class="nav-item">
+                            <a class="nav-link" href="#"><strong>HOME</strong></a>
+                        </li>
+                    
+                        <li class="nav-item">
+                            <a class="nav-link" href="#"><strong>ABOUT US</strong></a>
+                        </li>
+                        
+                        <li class="nav-item">
+                            <a class="nav-link" href="#"><strong>CONTACT</strong></a>
+                        </li>
+                        
+                        <li class="nav-item">
+                            <a class="nav-link" href="#"><strong>TESTIMONIALS</strong></a>
+                        </li>
+                    </ul> 
+                </div>
+            </div>
+        </div>
+        <!--End of Navbar-->    
+        
+        <!--Carousel-->
+        <div class="bd-example">
+          <div id="carouselExampleCaptions" class="carousel slide" data-ride="carousel">
+            <ol class="carousel-indicators">
+              <li data-target="#carouselExampleCaptions" data-slide-to="0" class="active"></li>
+              <li data-target="#carouselExampleCaptions" data-slide-to="1"></li>
+            </ol>
+            <div class="carousel-inner" role="listbox">
+              <div class="carousel-item active">
+                <img class="d-block img-fluid" src="img/capture.png" alt="First slide">
+                <div class="carousel-caption d-none d-md-block">
+                  
+                </div>
+              </div>
+              <div class="carousel-item">
+                <img class="d-block img-fluid" src="img/jumbo.png" alt="Second slide">
+                <div class="carousel-caption d-none d-md-block">
+                  <h1>Welcome To Biomine!</h1>
+                  <h3>The Company leading you to a healthy life!</h3>
+                  <button type='button' style='color: orange;'>See more products</button>
+                </div>
+              </div>
+            </div>
+            <a class="carousel-control-prev" href="#carouselExampleCaptions" role="button" data-slide="prev">
+              <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+              <span class="sr-only">Previous</span>
+            </a>
+            <a class="carousel-control-next" href="#carouselExampleCaptions" role="button" data-slide="next">
+              <span class="carousel-control-next-icon" aria-hidden="true"></span>
+              <span class="sr-only">Next</span>
+            </a>
+          </div>
+        </div>
+        <!--End of Carousel-->
+        
+        <main>
+            <div class="container">
+                <center>
+                    <div class="col-md-12" style="padding:2%;">
+                        <h2>Enjoy <strong>Our Best Sellers</strong></h2>
+                    </div>
+                    
+                    <p>Check out our best selling products developed by a talented team of researchers, nutritionists <br>
+                        and doctors. Whether you hope to boost your metabolism, protect your vital organs or <br>
+                        renew your love life, Biomine is here to help!</p>
+                
+                    <img src="img/Eau-de-rose.jpg" alt="symbol" style="padding: 15px;">
+                </center>
+            </div>
+            
+            
+            <section id="products">
+                <div class="container">
+                    <!--Beginning of first row-->
+                    <div class="w3-row-padding">
+                        <div class="w3-third w3-container w3-margin-bottom">
+                            <a href='GoFree.php'><img src="/img/GoFree.PNG" alt="Go Free" style="width:100%" class="w3-hover-opacity"></a>
+                          <div class="w3-container w3-white">
+                              <center><h3><b>Go Free</b></h3>
+                            <p>This proprietary blend of natural ingredients has been formulated to support joint health and flexibility. Containing 1500mg of Glucosamine
+                                which helps keep cartilage in joints healthy, improves recovery from sports injury. Chondroitin reduces pain and improves joint mobility</p></center>
+                          </div>
+                        </div>
+                        <div class="w3-third w3-container w3-margin-bottom">
+                          <a href='omega3.php'><img src="/img/Omega3.PNG" alt="Omega3" style="width:100%" class="w3-hover-opacity"></a>
+                          <div class="w3-container w3-white">
+                            <center><h3><b>Omega-3 Fish Oil Blend</b></h3>
+                            <p>Manufactured in a strict quality control environment our Omega 3 Fish Oil is free of all harmful contaminates. Using 100% pure sea-harvested pelagic 
+                                fish oil, making this supplement high in EPA and DHA. These nutrients are important for maintaining a healthy Brain and Nervous System and contributing
+                                to a healthy heart and Joint flexibility.</p></center>
+                          </div>
+                        </div>
+                        <div class="w3-third w3-container">
+                          <a href='Liver_Support.php'><img src="/img/LiverSupport.PNG" alt="Liver Support" style="width:100%" class="w3-hover-opacity"></a>
+                          <div class="w3-container w3-white">
+                              <center><h3><b>Liver Support</b></h3>
+                            <p>Liver is the largest internal organ and functions to filter the blood, cleansing toxins, wastes and pathogens. It also plays a key role in cholesterol
+                                balance, digestive enzyme production and energy metabolism. It is the main organ to burn fat. In addition, it is a resilient organ and is the only organ 
+                                capable of regeneration.</p></center>
+                          </div>
+                        </div>
+                    </div>
+                    <!--End of first row-->
+                    
+                    <!--Beginning of second row-->
+                    <div class="w3-row-padding">
+                        <div class="w3-third w3-container w3-margin-bottom">
+                            <a href='vita_for_men.php'><img src="/img/MenS.PNG" alt="Go Free" style="width:100%" class="w3-hover-opacity"></a>
+                          <div class="w3-container w3-white">
+                              <center><h3><b>Ultra Vita for Men</b></h3>
+                            <p>This vitamin contains essential vitamins, antioxidants and minerals. It also contains Beta Carotene which supports heart health. In additions
+                                to helping lower high blood pressure, it helps to relieve prostate, macular degeneration, asthma and arthritis symptoms.</p></center>
+                          </div>
+                        </div>
+                        <div class="w3-third w3-container w3-margin-bottom">
+                            <a href='vita_for_women.php'><img src="/img/WomenS.PNG" alt="Omega3" style="width:100%" class="w3-hover-opacity"></a>
+                          <div class="w3-container w3-white">
+                            <center><h3><b>Ultra Vita for Women</b></h3>
+                            <p>In today's fast-paced world, many women do not get the daily recommended allowance of vitamins, minerals and other nutrients. Vox Nutrition 
+                                helps to bridge the nutrient gap in your diet by offering a women's multivitamin that supports immune system, increase energy and mental 
+                                alertness to give your body with all that it needs.</p></center>
+                          </div>
+                        </div>
+                        <div class="w3-third w3-container">
+                            <a href='CoQ10.php'><img src="/img/CoQ10.PNG" alt="Liver Support" style="width:100%" class="w3-hover-opacity"></a>
+                          <div class="w3-container w3-white">
+                              <center><h3><b>CoQ10</b></h3>
+                            <p>Necessary for the function of cells, CoQ10 is a powerful antioxidant and vitamin-like compound. Supporting the protection of cell membranes,
+                                particularly after enduring surgery or taking anti-cancer medication, fighting off genetic diseases such as Huntington's Disease and muscular 
+                                dystrophy.</p></center>
+                          </div>
+                        </div>
+                    </div>
+                    <!--End of seecond row-->
+                    
+                    <!--Beginning of third row-->
+                    <div class="w3-row-padding">
+                        <div class="w3-third w3-container w3-margin-bottom">
+                            <a href='Probiotic.php'><img src="/img/Probiotic.PNG" alt="Go Free" style="width:100%" class="w3-hover-opacity"></a>
+                          <div class="w3-container w3-white">
+                              <center><h3><b>Probiotic-40</b></h3>
+                            <p>Probiotic 40 Billion is a blend of 4 strains (40 billion organisms) of Probiotic Bacteria. Our Probiotic supplement is designed to provide a 
+                                high potency and balance of beneficial bacteria. By taking this product on a regular basis Probiotic-40 can help maintain healthy intestinal 
+                                flora. This supplement also contains FOS to improve healthy growth of acidophilus and bifidus organisms.</p></center>
+                          </div>
+                        </div>
+                        <div class="w3-third w3-container w3-margin-bottom">
+                            <a href='Sleep_Formula.php'><img src="/img/SleepFormula.PNG" alt="Omega3" style="width:100%" class="w3-hover-opacity"></a>
+                          <div class="w3-container w3-white">
+                            <center><h3><b>Sleep Formula</b></h3>
+                            <p>This proprietary blend of natural ingredients include  melatonin, camomile and natural vitamin B-6. This natural blend has been formulated to 
+                                aid the body in obtaining a healthy amount of sleep. Sleep formula contains relaxing vitamins, minerals and herbs. When taken as directed, 
+                                sleep formula can help decrease chronic pain and reduce anxiety while falling asleep and sleeping through the night.</p></center>
+                          </div>
+                        </div>
+                    </div>
+                    <!--End of third row-->
+                    
+                </div>
+            </section>
+            
+            <div class="w3-display-container">
+                <img src="/img/pic.PNG" style="width:100%; height: 600px;">
+            </div>
+            
+        </main>
+        
+        <!-- Subscribe section -->
+        <div class="container-fluid w3-gray w3-padding-32">
+            <div class='container'>
+                <h1>Subscribe</h1>
+                <p>To get special offers and VIP treatment:</p>
+                <p><input class="w3-input w3-border" type="text" placeholder="Enter e-mail" style="width:70%"></p>
+                <button type="button" class="w3-button w3-red w3-margin-bottom">Subscribe</button>
+            </div>
+        </div>
+        
+        
+        
+        <!-- Footer -->
+        <footer class="w3-padding-64 w3-light-grey w3-small w3-center">
+            <div class='container'>
+                <div class="row">
+                   <div class="col-4">
+                        <h4>Contact</h4>
+                        <p>Questions? Go ahead.</p>
+                        <form action="/action_page.php" target="_blank">
+                          <p><input class="w3-input w3-border" type="text" placeholder="Name" name="Name" required></p>
+                          <p><input class="w3-input w3-border" type="text" placeholder="Email" name="Email" required></p>
+                          <p><input class="w3-input w3-border" type="text" placeholder="Subject" name="Subject" required></p>
+                          <p><textarea class="w3-input w3-border" type="text" placeholder="Message" name="Message" required></textarea></p>
+                          <button type="submit" class="w3-button w3-block w3-black">Send</button>
+                        </form>
+                    </div>
+
+                        <div class="col-4">
+                          <h4>About</h4>
+                          <p><a href="#">About us</a></p>
+                          <p><a href="#">We're hiring</a></p>
+                          <p><a href="#">Support</a></p>
+                          <p><a href="#">Find store</a></p>
+                          <p><a href="#">Shipment</a></p>
+                          <p><a href="#">Payment</a></p>
+                          <p><a href="#">Gift card</a></p>
+                          <p><a href="#">Return</a></p>
+                          <p><a href="#">Help</a></p>
+                        </div>
+
+                        <div class="col-4 w3-justify">
+                            <h4>Store</h4>
+                            <p><i class="fa fa-fw fa-map-marker"></i> Company Name</p>
+                            <p><i class="fa fa-fw fa-phone"></i> 0044123123</p>
+                            <p><i class="fa fa-fw fa-envelope"></i> ex@mail.com</p>
+                            <h4>We accept</h4>
+                            <p><i class="fa fa-fw fa-cc-amex"></i> Amex</p>
+                            <p><i class="fa fa-fw fa-credit-card"></i> Credit Card</p>
+                            <br>
+                            <a href="#" class="fa1 fa-facebook"></a>
+                            <a href="#" class="fa1 fa-twitter"></a>
+                            <a href="#" class="fa1 fa-google"></a>
+                            <a href="#" class="fa1 fa-linkedin"></a>
+                            <a href="#" class="fa1 fa-youtube"></a>
+                            <a href="#" class="fa1 fa-instagram"></a>
+                        </div>
+                </center>
+                       
+        </div>
+        </div>
+        </footer>
+    </body>
+</html>
